40c2ce880e70601b5aca2cab884aabf12ced2f6f
RodrigoDuvivier <rodrigo.martinez224@gmail.com>
2025-04-09 04:08:38 -0300

    v2

--- main.go
+++ main.go
@@ -9,5 +9,17 @@ import (
 
 
 func main() {
+	server := gin.Default()
 
+	server.GET("/events", getEvents ) //* get,post,put,delete,patch
+
+	server.Run(":8080") //* localhost:8080
+
+}
+
+
+func getEvents(c *gin.Context) {
+	// Logic to retrieve events from the database or any data source
+	events := []string{"Event 1", "Event 2", "Event 3"}
+	c.JSON(200, events)
 }
---
+++ models/event.go
@@ -0,0 +1,22 @@
+package models
+
+import "time"
+
+type Event struct {
+	ID          int    `json:"id"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	Location    string `json:"location"`
+	DateTime    time.Time `json:"date_time"`
+	UserID	 int    `json:"user_id"`
+
+}
+
+
+var events = []Event{}
+
+func (e Event) Save(){
+ // later : addd it to a database
+
+ events = append(events, e)
+}
